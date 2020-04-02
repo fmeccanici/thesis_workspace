90f2c0efefbaa4767e6d8cf4ddfc3f53c2763cf8
Floris <florismeccanici@gmail.com>
2020-04-02 17:53:12 +0200

    working video stream with both cameras

--- gui/nodes/gui.py
+++ gui/nodes/gui.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env/ python
+#!/usr/bin/env python2.7
 
 import rospy, sys, cv2
-import cv2.cv as cv
-from sensor_msgs import Image
+from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 
@@ -13,51 +12,61 @@ class GUI():
 
         rospy.on_shutdown(self.cleanup)
         self.cv_window_name = self.node_name
-        cv.NamedWindow(self.cv_window_name, cv.CV_WINDOW_NORMAL)
-        cv.MoveWindow(self.cv_window_name, 25, 75)
-
         self.bridge = CvBridge()
 
+        self.ee_frame = None
+        self.head_frame = None
+        self.concat_video = None
+    
 
         head_camera_sub_ = rospy.Subscriber('/xtion/rgb/image_raw', Image, self.head_camera_callback_)
         ee_camera_sub = rospy.Subscriber('/end_effector_cam/image_raw', Image, self.ee_camera_callback_)
         
         rospy.loginfo("Waiting for image topics...")    
     
+    def concatenate_video(self):
+        if self.ee_frame is not None and self.head_frame is not None:
+            width = 288
+            height = 512
+            dsize = (width, height)
+            
+            ee_image = cv2.resize(self.ee_frame, dsize)
+            head_image = cv2.resize(self.head_frame, dsize)
+
+            concat = np.hstack((ee_image, head_image))
+            cv2.imshow('Image panel', concat)
+            cv2.waitKey(20)
+
     def ee_camera_callback_(self, image):
 
-    def head_camera_callback_(self, image):  
         try:
-            frame = self.bridge.imgmsg_to_cv(image, "bgr8")
+            self.ee_frame = self.bridge.imgmsg_to_cv2(image, "bgr8")
         except CvBridgeError as e:
             print(e)
         
-        # Convert the image to a Numpy array since most cv2 functions
-        # require Numpy arrays.
-        frame = np.array(frame, dtype=np.uint8)
-        
-        # Display the image.
-        cv2.imshow(self.node_name, frame)
 
-        # Process any keyboard commands
-        self.keystroke = cv.WaitKey(5)
-        if 32 <= self.keystroke and self.keystroke < 128:
-            cc = chr(self.keystroke).lower()
-            if cc == 'q':
-                # The user has press the q key, so exit
-                rospy.signal_shutdown("User hit q key to quit.")
-    
+    def head_camera_callback_(self, image): 
+
+        try:
+            self.head_frame = self.bridge.imgmsg_to_cv2(image, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+        
     def cleanup(self):
         print("Shutting down vision node.")
         cv2.destroyAllWindows()   
     
-    def main(args):
-        try:
-            GUI()
-            rospy.spin()
-        except KeyboardInterrupt:
-            print("Shutting down vision node.")
-            cv.DestroyAllWindows()
+def main(args):
+    try:
+        gui = GUI()
+        r = rospy.Rate(30)
+        while not rospy.is_shutdown(): 
+                gui.concatenate_video()
+                r.sleep()
+    except KeyboardInterrupt:
+        print("Shutting down vision node.")
+        cv2.destroyAllWindows()
+    
 
-if __name__ == __main__:
+if __name__ == "__main__":
     main(sys.argv)
