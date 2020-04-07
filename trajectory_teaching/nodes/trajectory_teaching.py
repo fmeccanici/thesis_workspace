a058c3736ab1855af0f45c4988172ff4c79bfc96
Floris <florismeccanici@gmail.com>
2020-04-07 12:00:21 +0200

    commented out keyboard ;ublisher, work again with buttons of omni

--- trajectory_teaching/nodes/trajectory_teaching.py
+++ trajectory_teaching/nodes/trajectory_teaching.py
@@ -32,7 +32,7 @@ class trajectoryTeaching():
 
         self.EEtrajectory = []
 
-        self.end_effector_pose_wrt_marker_sub = rospy.Subscriber("/end_effector_pose_wrt_marker", PoseStamped, self._end_effector_pose_wrt_marker_callback)
+        # self.end_effector_pose_wrt_marker_sub = rospy.Subscriber("/end_effector_pose_wrt_marker", PoseStamped, self._end_effector_pose_wrt_marker_callback)
         # self.slave_control_state_sub = rospy.Subscriber("slave_control_state", ControlState, self._slave_control_state_callback)
         self.end_effector_pose_sub = rospy.Subscriber("/end_effector_pose", PoseStamped, self._end_effector_pose_callback)
 
@@ -60,11 +60,10 @@ class trajectoryTeaching():
 
     def _end_effector_pose_callback(self, data):
         self.current_slave_pose = data.pose
-
-    def _end_effector_pose_wrt_marker_callback(self, data):
+        
         if self.white_button_toggle_previous == 0 and self.white_button_toggle == 1:
             print("Appending trajectory")
-            self.marker_counter = 1
+            # self.marker_counter = 1
             # data.header.stamp.secs = data.header.stamp.secs
             # data.header.stamp.nsecs = data.header.stamp.nsecs
             data.header.stamp = rospy.Time.now()
@@ -77,6 +76,23 @@ class trajectoryTeaching():
              self.marker_pose.position.y, self.marker_pose.position.x, self.marker_pose.position.z,
              self.marker_pose.orientation.x, self.marker_pose.orientation.y, self.marker_pose.orientation.z, self.marker_pose.orientation.w, 
              data.header.stamp.secs, data.header.stamp.nsecs])
+
+    # def _end_effector_pose_wrt_marker_callback(self, data):
+    #     if self.white_button_toggle_previous == 0 and self.white_button_toggle == 1:
+    #         print("Appending trajectory")
+    #         self.marker_counter = 1
+    #         # data.header.stamp.secs = data.header.stamp.secs
+    #         # data.header.stamp.nsecs = data.header.stamp.nsecs
+    #         data.header.stamp = rospy.Time.now()
+
+
+    #         # marker x and y seem to be flipped wrt base_footprint
+    #         # therefore first position.y then position.x for marker_pose
+    #         self.EEtrajectory.append([data.pose.position.x,data.pose.position.y,data.pose.position.z,
+    #          data.pose.orientation.x,data.pose.orientation.y,data.pose.orientation.z,data.pose.orientation.w,
+    #          self.marker_pose.position.y, self.marker_pose.position.x, self.marker_pose.position.z,
+    #          self.marker_pose.orientation.x, self.marker_pose.orientation.y, self.marker_pose.orientation.z, self.marker_pose.orientation.w, 
+    #          data.header.stamp.secs, data.header.stamp.nsecs])
     # def _end_effector_pose_wrt_marker_callback(self,data):
         
     #     if self.white_button_toggle_previous == 0 and self.white_button_toggle == 1:
@@ -176,7 +192,7 @@ class trajectoryTeaching():
                 print("Saving trajectory data")
                 
                 # path = "/home/fmeccanici/Documents/thesis/lfd_ws/src/marco_lfd/data/raw/"
-                path = "/home/fmeccanici/Documents/thesis/lfd_ws/src/trajectory_teaching/data/marker_wrt_base_ee_wrt_marker/"
+                path = "/home/fmeccanici/Documents/thesis/lfd_ws/src/trajectory_teaching/data/both_wrt_base/"
 
                 print("file_name = " + self._get_trajectory_file_name(path))
                 file_name = self._get_trajectory_file_name(path)
@@ -188,7 +204,7 @@ class trajectoryTeaching():
                 self.white_button_toggle_previous = 0
 
                 # be able to detect new marker
-                self.marker_counter = 0
+                # self.marker_counter = 0
 
             r.sleep()
 
