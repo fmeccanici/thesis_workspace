86dbd961558101efb3fa5a637c4614cb30726572
Floris <florismeccanici@gmail.com>
2020-05-13 11:05:15 +0200

    working data to csv, proceed with gui for operator which contains participant class

--- data_logging/src/data_logger/data_logger.py
+++ data_logging/src/data_logger/data_logger.py
@@ -13,7 +13,7 @@ class ParticipantData(object):
             self.conditions[i+1] = { 
                             'predicted_trajectory': {'trajectory': {'x': [], 'y': [], 'z': [], 'qx': [],'qy': [], 'qz': [], 'qw': [], 't': [] }, 'context': []}, 
                             'refined_trajectories': {'trajectories': {}, 'contexts': {}, 'forces': {}}, 'number_of_refinement': 0,
-                            'objects_missed': 0, 'obstacles_hit': 0}
+                            'object_missed': 0, 'obstacles_hit': 0}
         
         self.refined_trajectories = {}
         self.refined_trajectory = {'trajectory': {'x': [], 'y': [], 'z': [], 'qx': [],'qy': [], 'qz': [], 'qw': [], 't': [] }, 'context': []}
@@ -26,7 +26,6 @@ class ParticipantData(object):
             # for i in range(3):
             #     os.makedirs(self.path + 'condition_' + str(i+1) + '/')
 
-        print(self.conditions[1])
 
     def setName(self, name):
         self.name = name
@@ -53,18 +52,7 @@ class ParticipantData(object):
         return self.path
 
     def addRefinedTrajectory(self, *args, **kwargs):
-        if "trajectory" in kwargs and "context" in kwargs and "condition" in kwargs:
-
-            # add refined trajectory 
-            self.refined_trajectories[condition]['trajectory'].append(trajectory)
-            self.refined_trajectories[condition]['context'].append(context)
-            
-            # increment number of refined trajectories
-            self.number_of_refinements[condition] += 1
-            
-            return 0
-            
-        elif "from_file" in kwargs and "condition" in kwargs:
+        if "from_file" in kwargs and "condition" in kwargs:
             condition = kwargs["condition"]
 
             path = '/home/fmeccanici/Documents/thesis/thesis_workspace/src/gui/data/experiment/'
@@ -85,16 +73,7 @@ class ParticipantData(object):
         return self.refined_trajectories[condition]['trajectories']
                 
     def setPredictedTrajectory(self, *args, **kwargs):
-        if "trajectory" in kwargs and "context" in kwargs and "condition" in kwargs:
-            condition = kwargs["condition"]
-
-            # add refined trajectory 
-            self.predicted_trajectory[condition]['trajectory'].append(trajectory)
-            self.predicted_trajectory[condition]['context'].append(context)
-            
-            return 0
-        
-        elif "from_file" in kwargs and kwargs["condition"] == 1 and "condition" in kwargs:
+        if "from_file" in kwargs and kwargs["condition"] == 1 and "condition" in kwargs:
             path = '/home/fmeccanici/Documents/thesis/thesis_workspace/src/gui/data/experiment/'
             file_name = 'predicted_trajectory.csv'
             
@@ -136,39 +115,20 @@ class ParticipantData(object):
         return self.conditions[condition]['predicted_trajectory']['trajectory']
 
     def incrementObstaclesHit(self, condition):
-        self.obstacles_hit[condition] += 1
+        self.conditions[condition]['obstacles_hit'] += 1
 
     def incrementObjectMissed(self, condition):
-        self.object_missed[condition] += 1
+        self.conditions[condition]['object_missed'] += 1
 
     def toCSV(self):
-        df = pd.DataFrame.from_dict(self.conditions)
+        data = {'number': self.number, 'age': self.age, 'gender': self.gender, 'condition': self.conditions}
+        df = pd.DataFrame.from_dict(data)
         df.to_csv(self.path + 'data.csv')
-        # print(df.head())
-        # file_names = ['condition_1.csv','condition_2.csv', 'condition_3.csv']
-        # column_names = ['ParticipantNumber', 'Gender', 'Age', 'RefinedTrajectories', 'PredictedTrajectory', 'NumberOfRefinements', 'Context',
-        #                 'ObstaclesHit', 'ObjectMissed', 'Forces']
-        # data = 'x, y, z, qx, qy, qz, qw, t \n'
-        # for i,file_name in enumerate(file_names):
-        #     data = ""
-        #     for column in column_names:
-        #         data += "%s, " % (column)
-        #     data += '\n'
-        #     try:
-        #         with open(self.path+file_name, 'w+') as csv_file:
-        #             data += "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s \n" % (self.number, self.gender, self.age, 
-        #                                                     self.refined_trajectories[i+1]['trajectories'], 
-        #                                                     self.predicted_trajectory[i+1]['trajectory'], 
-        #                                                     self.number_of_refinements[i+1], self.predicted_trajectory[i+1]['context'],
-        #                                                     self.obstacles_hit[i+1], self.object_missed[i+1], self.forces[i+1])
-        #             csv_file.write(data)
 
-        #     except IOError:
-        #         print("I/O error")
             
 if __name__ == "__main__":
     participant1_data = ParticipantData(1, 'm', 26)
-    # participant1_data.incrementObjectMissed(condition=1)
+    participant1_data.incrementObjectMissed(condition=1)
+    participant1_data.incrementObstaclesHit(condition=1)
     participant1_data.setPredictedTrajectory(condition=1, from_file=True)
     participant1_data.toCSV()
-    # print(participant1_data.getPredictedTrajectory(condition=1))
