eb116d69160a4b030c83699dd8ebfa2915137c63
Floris <florismeccanici@gmail.com>
2020-07-14 11:00:16 +0200

    working failure detection, both object reaching and obstacle collision

--- execution_failure_detection/nodes/execution_failure_detection_node.py
+++ execution_failure_detection/nodes/execution_failure_detection_node.py
@@ -1,68 +1,86 @@
 import rospy, os, tf
 from visualization_msgs.msg import Marker, MarkerArray
-from std_msgs.msg import Header, ColorRGBA
+from std_msgs.msg import Header, ColorRGBA, Bool
 from geometry_msgs.msg import Vector3, Pose, PoseStamped
 import numpy as np
 import copy
 from pyquaternion import Quaternion
 
+from gazebo_msgs.msg import LinkStates
+from execution_failure_detection.srv import GetExecutionFailure, GetExecutionFailureResponse
+
 class ExecutionFailureNode(object):
     def __init__(self, model_path='/home/fmeccanici/Documents/thesis/thesis_workspace/src/execution_failure_detection/models/'):
         rospy.init_node('execution_failure_detection_node')
-        self.model_path = model_path
-        self.models = {}
-        self.model_pub = rospy.Publisher('execution_failure_detection/model_visualization', MarkerArray, queue_size=10)
-        self.ee_sub = rospy.Subscriber('end_effector_pose', PoseStamped, self._EffectorPoseCallback)
-        self.listener = tf.TransformListener()
-        
-        self.frame_id = 'base_footprint'
-
-        self.poses = []
+        self.table_pose_counter = 0
+        # set object/table sizes 
 
         ##### true size ######
-        x = 0.8
-        y = 1.5
-        z = 0.66
+        self.table_size_x = 0.8
+        self.table_size_y = 1.5
+        self.table_size_z = 0.66
 
         ##### size adapted for collision ######
         # x = 0.8
         # y = 1.5
         # z = 0.7
 
-        pose = Pose()
-        pose.position.x = 0.478747 + x/2
-        pose.position.y = -0.660374 + y/2
-        pose.position.z = 0.0 + z/2
+        self.object_size_x = 0.05
+        self.object_size_y = 0.05
+        self.object_size_z = 0.1
 
-        self.xmin, self.xmax = pose.position.x - x/2, pose.position.x + x/2
-        self.ymin, self.ymax = pose.position.y - y/2, pose.position.y + y/2
-        self.zmin, self.zmax = pose.position.z - z/2, pose.position.z + z/2
+        self.frame_id = 'base_footprint'
 
-        color = 'red'
+        self.model_path = model_path
+        self.models = {}
+        self.model_pub = rospy.Publisher('execution_failure_detection/model_visualization', MarkerArray, queue_size=10)
+        self.ee_sub = rospy.Subscriber('end_effector_pose', PoseStamped, self._endEffectorPoseCallback)
+        self.link_states_sub = rospy.Subscriber('/gazebo/link_states', LinkStates, self.linkStatesCallback)
 
-        self.poses.append(pose)
- 
-        self.addCube(x, y, z, pose, color)
+        self.execution_failure_service = rospy.Service('get_execution_failure', GetExecutionFailure, self._getExecutionFailure)
 
-        x = 0.05
-        y = 0.05
-        z = 0.1
+        self.listener = tf.TransformListener()
+        
 
-        pose = Pose()
-        pose.position.x = 0.81999491302 
-        pose.position.y = 0.299999273129 
-        pose.position.z = 0.708 
-        color = 'blue'
-
-        self.poses.append(pose)
-
-        self.addCube(x,y,z,pose, color)
-    
-    def _EffectorPoseCallback(self, data):
+    def _endEffectorPoseCallback(self, data):
         self.ee_pose = data.pose
         # print(self.isObstacleHit(self.ee_position, self.xmin,self.xmax, self.ymin, self.ymax,self.zmin,self.zmax))
 
-    def setEECubeSize(self):
+    def _getExecutionFailure(self, req):
+        resp = GetExecutionFailureResponse()
+        object_reached = Bool()
+        obstacle_hit = Bool()
+        object_reached.data = self.isObjectReached()
+        obstacle_hit.data = self.isObstacleHit()
+
+        resp.object_reached = object_reached
+        resp.obstacle_hit = obstacle_hit
+
+        return resp
+
+
+    def linkStatesCallback(self, data):
+        self.object_pose = data.pose[1]
+        self.table_pose = data.pose[2]
+                
+        if self.table_pose_counter == 0:
+            color = 'red'
+            self.table_pose.position.x = self.table_pose.position.x + self.table_size_x/2
+            self.table_pose.position.y = self.table_pose.position.y + self.table_size_y/2
+            self.table_pose.position.z = self.table_pose.position.z + self.table_size_z/2
+
+            # self.xmin, self.xmax = table_pose.position.x - self.table_size_x/2, table_pose.position.x + self.table_size_x/2
+            # self.ymin, self.ymax = table_pose.position.y - self.table_size_y/2, table_pose.position.y + self.table_size_y/2
+            # self.zmin, self.zmax = table_pose.position.z - self.table_size_z/2, table_pose.position.z + self.table_size_z/2
+        
+            self.addCube(self.table_size_x, self.table_size_y, self.table_size_z, self.table_pose, color, 1)
+        
+        color = 'blue'
+        self.addCube(self.object_size_x, self.object_size_y, self.object_size_z, self.object_pose, color, 2)
+
+
+    def setEllipsoidSize(self, ellipsoid_type='collision'):
+
         while True:    
             try:
                 if self.listener.canTransform('/gripper_finger_tip_left_link', self.frame_id, rospy.Time(0)) and self.listener.canTransform('/gripper_finger_tip_right_link', self.frame_id, rospy.Time(0)):
@@ -74,52 +92,111 @@ class ExecutionFailureNode(object):
                     (left_finger_tip_trans,left_finger_tip_rot) = self.listener.lookupTransform(self.frame_id,'/gripper_finger_tip_left_link', rospy.Time(0))
                     (right_finger_tip_trans,right_finger_tip_rot) = self.listener.lookupTransform(self.frame_id, '/gripper_finger_tip_right_link', rospy.Time(0))
 
-                    self.size_x = abs(left_finger_tip_trans[0] - self.ee_pose.position.x) + 0.2
-                    self.size_y = abs(left_finger_tip_trans[1] - right_finger_tip_trans[1]) + 0.1
-                    self.size_z = 0.1 # 0.1 is best
-                    
+                    if ellipsoid_type == 'collision':
+                        self.collision_ellipsoid_size_x = abs(left_finger_tip_trans[0] - self.ee_pose.position.x) + 0.2
+                        self.collision_ellipsoid_size_y = abs(left_finger_tip_trans[1] - right_finger_tip_trans[1]) + 0.1
+                        self.collision_ellipsoid_size_z = 0.1 # 0.1 is best
+
+                    elif ellipsoid_type == 'reaching':
+                        self.reaching_ellipsoid_size_x = abs(left_finger_tip_trans[0] - self.ee_pose.position.x) - 0.08
+                        self.reaching_ellipsoid_size_y = abs(left_finger_tip_trans[1] - right_finger_tip_trans[1]) + 0.03
+                        self.reaching_ellipsoid_size_z = 0.1 
+
+                    elif ellipsoid_type == 'all':
+                        self.collision_ellipsoid_size_x = abs(left_finger_tip_trans[0] - self.ee_pose.position.x) + 0.2
+                        self.collision_ellipsoid_size_y = abs(left_finger_tip_trans[1] - right_finger_tip_trans[1]) + 0.1
+                        self.collision_ellipsoid_size_z = 0.1 # 0.1 is best
+
+                        self.reaching_ellipsoid_size_x = abs(left_finger_tip_trans[0] - self.ee_pose.position.x) - 0.08
+                        self.reaching_ellipsoid_size_y = abs(left_finger_tip_trans[1] - right_finger_tip_trans[1]) + 0.03
+                        self.reaching_ellipsoid_size_z = 0.1 
                     break
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                 pass
-    
-    def isCollidingWithXYPlane(self, xmin, xmax, ymin, ymax, z):
 
+    def setEllipsoidOrigin(self, ellipsoid_type='collision'):
+        if ellipsoid_type == 'collision':
+            self.collision_ellipsoid_origin = copy.deepcopy(self.ee_pose)
+            
+            self.collision_ellipsoid_origin.position.x += 0.04
+            self.collision_ellipsoid_origin.position.y -= 0.01
+            self.collision_ellipsoid_origin.position.z -= 0.01  
+        
+        elif ellipsoid_type == 'reaching':
+            self.reaching_ellipsoid_origin = copy.deepcopy(self.ee_pose)
+
+            self.reaching_ellipsoid_origin.position.x += 0.12
+            self.reaching_ellipsoid_origin.position.y -= 0.025
+            self.reaching_ellipsoid_origin.position.z -= 0.03
+
+        elif ellipsoid_type == 'all':
+            self.collision_ellipsoid_origin = copy.deepcopy(self.ee_pose) 
+            self.reaching_ellipsoid_origin = copy.deepcopy(self.ee_pose)
+            
+            self.collision_ellipsoid_origin.position.x += 0.04
+            self.collision_ellipsoid_origin.position.y -= 0.01
+            self.collision_ellipsoid_origin.position.z -= 0.01  
+
+            self.reaching_ellipsoid_origin.position.x += 0.12
+            self.reaching_ellipsoid_origin.position.y -= 0.025
+            self.reaching_ellipsoid_origin.position.z -= 0.03
+
+    def isObstacleHit(self):
+        # z = self.table_pose.position.z
+        # seems to be an offset in the z direction 
+        # needed to properly detect if it is hitting the table
         z = 0.59
+        xmin, xmax = self.table_pose.position.x - self.table_size_x/2, self.table_pose.position.x + self.table_size_x/2
+        ymin, ymax = self.table_pose.position.y - self.table_size_y/2, self.table_pose.position.y + self.table_size_y/2
+        
         for x in np.arange(xmin, xmax, 0.1):
             for y in np.arange(ymin, ymax, 0.1):
-                if self.isInsideEllipsoid(x, y, z):
+                if self.isInsideEllipsoid(x, y, z, ellipsoid_type='collision'):
+                    return True
+
+        return False
+    
+    def isObjectReached(self):
+        x = self.object_pose.position.x 
+        ymin, ymax = self.object_pose.position.y - self.object_size_y/2, self.object_pose.position.y + self.object_size_y/2
+        zmin, zmax = self.object_pose.position.z - self.object_size_z/2, self.object_pose.position.z + self.object_size_z/2
+
+        for y in np.arange(ymin, ymax, 0.01):
+            for z in np.arange(zmin, zmax, 0.01):
+                if self.isInsideEllipsoid(x, y, z, ellipsoid_type='reaching'):
                     return True
 
         return False
 
-    def isInsideEllipsoid(self, x, y, z):
+
+    def isInsideEllipsoid(self, x, y, z, ellipsoid_type='collision'):
         q_ee = Quaternion(copy.deepcopy([self.ee_pose.orientation.w, self.ee_pose.orientation.x, self.ee_pose.orientation.y, self.ee_pose.orientation.z]))
 
-        # ellipsoid centre
-        origin = copy.deepcopy(self.ee_pose)
-        
-        origin.position.x += 0.04
-        origin.position.y -= 0.01
-        origin.position.z -= 0.01
+        if ellipsoid_type == 'collision':
 
-        # rotate ellipsoid
-        vec_rotated = q_ee.rotate([(x - origin.position.x), (y - origin.position.y), (z - origin.position.z)])
+            # rotate ellipsoid
+            vec_rotated = q_ee.rotate([(x - self.collision_ellipsoid_origin.position.x), (y - self.collision_ellipsoid_origin.position.y), (z - self.collision_ellipsoid_origin.position.z)])
+            f = (vec_rotated[0] / self.collision_ellipsoid_size_x)**2 + (vec_rotated[1] / self.collision_ellipsoid_size_y)**2 + (vec_rotated[2] / self.collision_ellipsoid_size_z)**2 
 
-        # f = ((x - origin.position.x) / self.size_x)**2 + ((y - origin.position.y) / self.size_y)**2 + ((z - origin.position.z) / self.size_z)**2 
-        f = (vec_rotated[0] / self.size_x)**2 + (vec_rotated[1] / self.size_y)**2 + (vec_rotated[2] / self.size_z)**2 
+        elif ellipsoid_type == 'reaching':
 
-        # print(vec_rotated)
+            # rotate ellipsoid
+            vec_rotated = q_ee.rotate([(x - self.reaching_ellipsoid_origin.position.x), (y - self.reaching_ellipsoid_origin.position.y), (z - self.reaching_ellipsoid_origin.position.z)])
+            f = (vec_rotated[0] / self.reaching_ellipsoid_size_x)**2 + (vec_rotated[1] / self.reaching_ellipsoid_size_y)**2 + (vec_rotated[2] / self.reaching_ellipsoid_size_z)**2 
+
         return f < 1
 
 
-    def addEECube(self):
-        id = 999
-        pose = Pose()
-        pose = self.ee_pose
-        pose.position.x += 0.04
-        pose.position.y -= 0.01
-        pose.position.z -= 0.01
-        
+    def deleteEllipsoid(self, ellipsoid_type='reaching'):
+        if ellipsoid_type == 'reaching':
+            id = 1000
+            pose = self.reaching_ellipsoid_origin
+            scale = Vector3(self.reaching_ellipsoid_size_x, self.reaching_ellipsoid_size_y, self.reaching_ellipsoid_size_z)
+        elif ellipsoid_type == 'collision':
+            id = 999
+            pose = self.collision_ellipsoid_origin
+            scale = Vector3(self.collision_ellipsoid_size_x, self.collision_ellipsoid_size_y, self.collision_ellipsoid_size_z)
+
         color=ColorRGBA(r=0, g=1, b=0, a=0.5)
 
         cube = Marker(header=Header(stamp=rospy.Time.now(),
@@ -128,18 +205,85 @@ class ExecutionFailureNode(object):
                                         type=Marker.SPHERE,
                                         color=color,
                                         id=id,
-                                        scale=Vector3(self.size_x, self.size_y, self.size_z),
-                                        action=Marker.ADD)
+                                        scale=Vector3(self.reaching_ellipsoid_size_x, self.reaching_ellipsoid_size_y, self.reaching_ellipsoid_size_z),
+                                        action=Marker.DELETE)
 
         self.models[id] = cube
 
-    def addCube(self, x, y, z, pose, color):
+    def addEllipsoid(self, ellipsoid_type='collision'):
+
+        if ellipsoid_type == 'collision':
+            id = 999
+
+            color=ColorRGBA(r=0, g=1, b=0, a=0.5)
+
+            cube = Marker(header=Header(stamp=rospy.Time.now(),
+                                            frame_id=self.frame_id),
+                                            pose=self.collision_ellipsoid_origin,
+                                            type=Marker.SPHERE,
+                                            color=color,
+                                            id=id,
+                                            scale=Vector3(self.collision_ellipsoid_size_x, self.collision_ellipsoid_size_y, self.collision_ellipsoid_size_z),
+                                            action=Marker.ADD)
+
+            self.models[id] = cube
+
+            self.deleteEllipsoid(ellipsoid_type='reaching')
+
+        elif ellipsoid_type == 'reaching':
+            id = 1000
+
+            color=ColorRGBA(r=1, g=0, b=1, a=1)
+
+            cube = Marker(header=Header(stamp=rospy.Time.now(),
+                                            frame_id=self.frame_id),
+                                            pose=self.reaching_ellipsoid_origin,
+                                            type=Marker.SPHERE,
+                                            color=color,
+                                            id=id,
+                                            scale=Vector3(self.reaching_ellipsoid_size_x, self.reaching_ellipsoid_size_y, self.reaching_ellipsoid_size_z),
+                                            action=Marker.ADD)
+
+            self.models[id] = cube
+            self.deleteEllipsoid(ellipsoid_type='collision')
+        
+        elif ellipsoid_type == 'all':
+            id = 999
+            
+            color=ColorRGBA(r=0, g=1, b=0, a=0.5)
+
+            cube = Marker(header=Header(stamp=rospy.Time.now(),
+                                            frame_id=self.frame_id),
+                                            pose=self.collision_ellipsoid_origin,
+                                            type=Marker.SPHERE,
+                                            color=color,
+                                            id=id,
+                                            scale=Vector3(self.collision_ellipsoid_size_x, self.collision_ellipsoid_size_y, self.collision_ellipsoid_size_z),
+                                            action=Marker.ADD)
+
+            self.models[id] = cube
+
+            id = 1000
+
+            color=ColorRGBA(r=1, g=0, b=1, a=1)
+
+            cube = Marker(header=Header(stamp=rospy.Time.now(),
+                                            frame_id=self.frame_id),
+                                            pose=self.reaching_ellipsoid_origin,
+                                            type=Marker.SPHERE,
+                                            color=color,
+                                            id=id,
+                                            scale=Vector3(self.reaching_ellipsoid_size_x, self.reaching_ellipsoid_size_y, self.reaching_ellipsoid_size_z),
+                                            action=Marker.ADD)
+
+            self.models[id] = cube
+            
+    def addCube(self, x, y, z, pose, color, id):
         if color == 'red':
             color=ColorRGBA(r=1, g=0, b=0, a=1)
         elif color == 'blue':
             color=ColorRGBA(r=0, g=0, b=1, a=1)
 
-        id = len(self.models)+1
         cube = Marker(header=Header(stamp=rospy.Time.now(),
                                         frame_id=self.frame_id),
                                         pose=pose,
@@ -157,46 +301,26 @@ class ExecutionFailureNode(object):
             model_list.append(self.models[id])
 
         self.model_pub.publish(model_list)
-        
-    def isObstacleHit(self, point, xmin, xmax, ymin, ymax, zmin, zmax):
-        x = point.x
-        y = point.y
-        z = point.y
-        
-        point = np.array([x,y,z])
+    
 
-        x_obstacle = self.poses[0].position.x
-        y_obstacle = self.poses[0].position.y
-        z_obstacle = self.poses[0].position.z
-
-        x_projection = point * np.array([x_obstacle, 0, 0])
-        y_projection = point * np.array([0, y_obstacle, 0])
-        z_projection = point * np.array([0, 0, z_obstacle])
-
-        # if (x_projection[0] < xmax and x_projection[0] > xmin) and (y_projection[1] < ymax and y_projection[1] > ymin) and (z_projection[2] < zmax and z_projection[2] > zmin):
-        #     return True
-        # else:
-        #     return False
-        
-        if (z_projection[2] < zmax and z_projection[2] > zmin):
-            return True
-        else:
-            return False
-
-    def isObjectReached(self):
-        return
+    # def isObstacleHit(self):
+    #     return self.isCollidingWithXYPlane(self.xmin, self.xmax, self.ymin, self.ymax, self.zmax)
+    
 
     def run(self):
         r = rospy.Rate(30)
-        self.setEECubeSize()
+        self.setEllipsoidSize(ellipsoid_type='all')
         # x = self.ee_pose.position.x
         # y = self.ee_pose.position.y
         # z = self.ee_pose.position.z 
 
         while not rospy.is_shutdown():
-            self.addEECube()
+            self.setEllipsoidOrigin(ellipsoid_type='all')
+            self.addEllipsoid(ellipsoid_type='all')
             self.visualizeModels()
-            print(self.isCollidingWithXYPlane(self.xmin, self.xmax, self.ymin, self.ymax, self.zmax))
+            print("obstacle hit: " + str(self.isObstacleHit()))
+            print("object reached: " + str(self.isObjectReached()))
+            print('\n')
             r.sleep()
 
 if __name__ == "__main__":
